9361f0d03d1d4ee5abe6cf38bacddd4074969f82
Vinayyadav296 <104241480+Vinayyadav296@users.noreply.github.com>
2022-08-29 18:11:04 +0530

    Add files via upload

---
+++ Trees/BinaryTree/DiameterOfTree.java
@@ -0,0 +1,59 @@
+package Trees.BinaryTree;
+
+public class DiameterOfTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        static Node buildTree(int node[]) {
+            idx++;
+            if (node[idx] == -1) {
+                return null;
+            }
+            Node newnode = new Node(node[idx]);
+            newnode.left = buildTree(node);
+            newnode.right = buildTree(node);
+            return newnode;
+        }
+    }
+
+    static int heightOfTree(Node root) {
+        if (root == null) { // base case for recursive function
+            return 0;
+        }
+        int leftnode = heightOfTree(root.left); // this will calculate the height of the left subtree
+        int rightnode = heightOfTree(root.right); // this will calculate the height of the right subtree
+        int height = Math.max(leftnode, rightnode) + 1; // this calculate the total height of tree
+        return height;
+    }
+
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int diam1 = heightOfTree(root.left) + heightOfTree(root.right) + 1;
+        int diam2 = diameter(root.left);
+        int diam3 = diameter(root.right);
+
+        return Math.max(diam1, Math.max(diam2, diam3));
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(diameter(root));
+    }
+}
